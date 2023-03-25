1108f0573728b41a76c387dc3e68e97ec9956175
NazimMakhmutkhodzhaev <73776399+NazimMakhmutkhodzhaev@users.noreply.github.com>
2023-03-25 08:41:18 +0600

    Update viewandedit.spec.js

--- test/specs/viewandedit.spec.js
+++ test/specs/viewandedit.spec.js
@@ -1,11 +1,11 @@
-const LoginPage = require("../pageobjects/loginpage");
-const ViewAndEditPage = require("../pageobjects/viewandeditpage");
+const LoginPage = require("../pageobjects/LoginPage");
+const ViewAndEditPage = require("../pageobjects/ViewAndEditPage");
 
 describe('Edit suit', () => {
     it('Edit client', async () => {
         await LoginPage.open();
         await LoginPage.doLogin('Admin', 'Admin@Navi');
 
-        await ViewAndEditPage.doEdit('Go D.', 'marie', '03.05.2001', '996558645984', 'marie@gmail.com');
+        await ViewAndEditPage.doEdit('Ann', 'Marie', '03.05.2001', '996558645984', 'marie@gmail.com');
     })
 })
